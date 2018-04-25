860299fb8356fc78e541a0f837b6d158c3402bca
OlivierLemoine <OlivierLemoine@users.noreply.github.com>
2018-04-25 20:15:58 +0200

    parse osm xml data to json data begining

--- Simbus/main.qml
+++ Simbus/main.qml
@@ -1,9 +1,61 @@
 import QtQuick 2.9
 import QtQuick.Window 2.2
+import QtQuick.Controls 1.4
 
 Window {
     visible: true
-    width: 640
-    height: 480
+    width: 150
+    height: 50
     title: qsTr("Hello World")
+    Button{
+        x: 0
+        y: 0
+        width: 150
+        height: 50
+        text: "API osm test"
+        onClicked: {
+
+            function parseData(data){
+                let begining = data.indexOf('<osm>');
+                let ending = data.indexOf('</osm>');
+                data = data.slice(begining, ending);
+
+                let res = '';
+
+                let isInsideTag = false;
+
+                let i = 0;
+                while(i < data.length){
+                    if(data[i] === '<'){
+                        isInsideTag = true;
+                        i++;
+                        let size = data.indexOf(' ', i);
+                        res.push('{' + data.slice(i, i + size) + ',');
+                        i += size;
+                    }
+                    else if(data[i] === '>'){
+                        isInsideTag = false;                  
+                    }
+
+                    i++;
+                }
+                return res;
+            }
+
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if ( xhr.readyState == xhr.DONE )
+                {
+                    // var jsonObject = JSON.parse(xhr.responseText);
+                    // console.log(jsonObject.toString());
+                    // console.log(JSON.parse('{"data": "test"}').data);
+                    // console.log(parseData(xhr.responseText));
+                    console.log(parseData(a));
+                }
+            }
+            var url = 'http://api.openstreetmap.org/api/0.6/map?bbox=-4.6388600,48.3521900,-4.6363600,48.3531700';
+            xhr.open('GET', url, true);
+            xhr.send();
+        }
+    }
 }
--- osmReqTest/index.js
+++ osmReqTest/index.js
@@ -1,19 +1,50 @@
-const https = require('https');
+function parseData(data) {
+    let begining = data.indexOf('<osm');
+    let ending = data.indexOf('</osm>');
+    data = data.slice(begining, ending + 6);
 
-https
-  .get('https://lemoineolivier35@gmail.com:VyZw9dWWj%&cmVlTAuvGJKbmyaW&8d@api.openstreetmap.org/api/0.6/permissions/', resp => {
-    let data = '';
+    let res = '';
 
-    // A chunk of data has been recieved.
-    resp.on('data', chunk => {
-      data += chunk;
-    });
+    let isInsideTag = false;
 
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(JSON.parse(data).explanation);
-    });
-  })
-  .on('error', err => {
-    console.log('Error: ' + err.message);
-  });
+    let i = 0;
+    while (i < data.length) {
+        if (data[i] === '<') {
+            isInsideTag = true;
+            let size = data.indexOf(' ', i);
+            res += '{"' + data.slice(i + 1, size) + '":[';
+            i = size;
+        } else if (data[i] === '>') {
+            if (data[i - 1] === '/') {
+                res += ']';
+            } else {
+                res += 'child:[';
+            }
+            isInsideTag = false;
+        } else if (isInsideTag) {
+            if (data[i] != ' ') {
+                let size = data.indexOf('=', i);
+                res += '"' + data.slice(i, size) + '"';
+                i = size;
+                size = data.indexOf('"', i + 2);
+                res += data.slice(i, size + 1);
+                res += ',';
+                i = size;
+            }
+        }
+
+        i++;
+    }
+    return res;
+}
+
+var a = `<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="CGImap 0.6.0 (9711 thorn-01.openstreetmap.org)" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
+    <bounds minlat="48.3521900" minlon="-4.6388600" maxlat="48.3531700" maxlon="-4.6363600"/>
+    <node id="1186380318" visible="true" version="1" changeset="7458074" timestamp="2011-03-04T21:21:18Z" user="FVig" uid="187752" lat="48.3552519" lon="-4.6331707"/>
+    <way id="135965285" visible="true" version="4" changeset="54211234" timestamp="2017-11-30T17:20:42Z" user="Olyon" uid="1443767">
+        <nd ref="1186372969"/>
+    </way>
+</osm>`;
+
+console.log(parseData(a));
