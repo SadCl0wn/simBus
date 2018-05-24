16a5f8e734b323211848e7d296fcc76855755192
Nagatsuna <38032554+Nagatsuna@users.noreply.github.com>
2018-05-24 16:15:54 +0200

    ajout interface

--- Simbus/sources/main.cpp
+++ Simbus/sources/main.cpp
@@ -1,14 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "../headers/parametres.hpp"
+#include "../headers/markermodel.hpp"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    MarkerModel markermodel;
+    markermodel.setAddAction(false);
 
     QGuiApplication app(argc, argv);
-
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    qmlRegisterType<Parametres>("Parametres", 1,0, "Parametres");
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("markermodel", &markermodel);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
     if (engine.rootObjects().isEmpty())
         return -1;
 
