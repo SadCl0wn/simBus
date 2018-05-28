1f1190e46876a7f33578d2aabf7088623416243e
MORILLON Francois <f5morill@raisin7.enib.fr>
2018-05-28 09:32:36 +0200

    pathfinding

--- Simbus/headers/dynamique.hpp
+++ Simbus/headers/dynamique.hpp
@@ -7,13 +7,14 @@
 
 class Dynamique:public Object{
     public:
-        Dynamique(float x=0,float y=0,float speed_x=0,float speed_y=0): Object(x,y),speed_(speed_x,speed_y){};
+        Dynamique(float x=0,float y=0,float speed_max=0): Object(x,y),speed_(0,0),max_speed(speed_max){};
         vec2<float> getSpeed();
         void setSpeed(float speed[2]); 
         void setSpeed(vec2<float> speed);
         void pathfinding(vec2<float> objectif);
     private:
         vec2<float> speed_;
+        float   max_speed;
         std::vector<vec2<float>> parcours;
 };
 
--- Simbus/sources/dynamique.cpp
+++ Simbus/sources/dynamique.cpp
@@ -1,18 +1,66 @@
 #include "../headers/dynamique.hpp"
 #include <limits.h>
+#include <algorithm>
 #include <math.h>
 
-struct routePath{
+struct RoutePath{
+    bool addAccessible(std::vector<RoutePath>& accesible,float speed_object);
     Route *r;
     unsigned int temps;
+    vec2<float> precedent;
 };
+
+bool RoutePath::addAccessible(std::vector<RoutePath>& accesible,float speed_object)
+{
+    RoutePath routeP;
+    for(auto route : this->r->getConnexionDepart())
+    {
+        routeP.r = route;
+        routeP.temps = this->temps;
+        routeP.precedent = this->r->getCoordDepart();
+        bool aHit = false;
+        std::for_each(accesible.beging(),accesible.end(),[routeP,&aHit](RoutePath &a){
+                                                                                    if(a.r == routeP.r)
+                                                                                    {
+                                                                                        aHit = true;
+                                                                                        if(a.temps > routeP.temps)
+                                                                                        {
+                                                                                            a.precedent = route.precedent;
+                                                                                            a.temps = routeP.temps;
+                                                                                        }
+                                                                                     }
+                                                                                });
+        if(!aHit)
+            accesible.push_back(routeP);
+    }
+    for(auto route : this->r->getConnexionArrive())
+    {
+        routeP.r = route;
+        float routeSpeed = route->getSpeed();
+        routeP.temps = this->temps+this->r->getDistance()/((routeSpeed<speed_object)?routeSpeed:speed_object);
+        routeP.precedent = this->r->getCoordDepart();
+        std::for_each(accesible.beging(),accesible.end(),[routeP,&aHit](RoutePath &a){
+                                                                                    if(a.r == routeP.r)
+                                                                                    {
+                                                                                        aHit = true;
+                                                                                        if(a.temps > routeP.temps)
+                                                                                        {
+                                                                                            a.precedent = route.precedent;
+                                                                                            a.temps = routeP.temps;
+                                                                                        }
+                                                                                     }
+                                                                                });
+        if(!aHit)
+            accesible.push_back(routeP);
+    }
+}
 
 void Dynamique::pathfinding(vec2<float> objectif)
 {
-    std::vector<routePath> accesible;
-    routePath plusProche{nullptr,UINT_MAX};
+    std::vector<RoutePath> accesible;
     vec2<float> coord_actuel = this->getCoor();
     vec2<float> a;
+    RoutePath plusProche{nullptr,UINT_MAX,coord_actuel};
     unsigned int t=0;
     for(auto route : Route::AllRoute)
     {
@@ -25,20 +73,7 @@ void Dynamique::pathfinding(vec2<float> objectif)
         }
     }
     this->parcours.push_back(plusProche.r->getCoordDepart());
-    routePath routeP;
-    for(auto route : plusProche.r->getConnexionDepart())
-    {
-        routeP.r = route;
-        routeP.temps = t;
-        accesible.push_back(routeP);
-    }
-    for(auto route : plusProche.r->getConnexionArrive())
-    {
-        routeP.r = route;
-        routeP.temps = t+plusProche.r->getDistance()/route->getSpeed();
-        accesible.push_back(routeP);
-        accesible.push_back(routeP);
-    }
+    plusProche.addAccessible(accesible,this->max_speed);
 }
 
 vec2<float> Dynamique::getSpeed()
