cca2eabc15d692bd9fc06e81da8e02aff37a06f5
MORILLON Francois <f5morill@laurier20.enib.fr>
2018-05-28 12:02:08 +0200

    avancement

--- Simbus/sources/dynamique.cpp
+++ Simbus/sources/dynamique.cpp
@@ -4,22 +4,24 @@
 #include <math.h>
 
 struct RoutePath{
-    bool addAccessible(std::vector<RoutePath>& accesible,float speed_object);
+    bool addAccessible(std::vector<RoutePath>& accessible,,float speed_object);
     Route *r;
     unsigned int temps;
     vec2<float> precedent;
 };
 
-bool RoutePath::addAccessible(std::vector<RoutePath>& accesible,float speed_object)
+bool RoutePath::addAccessible(std::vector<RoutePath>& accessible,std::vector<RoutePath>& parcourue,float speed_object)
 {
     RoutePath routeP;
     for(auto route : this->r->getConnexionDepart())
     {
+        if(route == this->r)
+            continue;
         routeP.r = route;
         routeP.temps = this->temps;
         routeP.precedent = this->r->getCoordDepart();
         bool aHit = false;
-        std::for_each(accesible.beging(),accesible.end(),[routeP,&aHit](RoutePath &a){
+        std::for_each(accessible.beging(),accessible.end(),[routeP,&aHit](RoutePath &a){
                                                                                     if(a.r == routeP.r)
                                                                                     {
                                                                                         aHit = true;
@@ -30,16 +32,19 @@ bool RoutePath::addAccessible(std::vector<RoutePath>& accesible,float speed_obje
                                                                                         }
                                                                                      }
                                                                                 });
+        std::for_each(parcourue.beging(),parcourue.end(),[routeP,&aHit](RoutePath &a){if(a.r == routeP.r)aHit = true;});
         if(!aHit)
-            accesible.push_back(routeP);
+            accessible.push_back(routeP);
     }
     for(auto route : this->r->getConnexionArrive())
     {
+        if(route == this->r)
+            continue;
         routeP.r = route;
         float routeSpeed = route->getSpeed();
         routeP.temps = this->temps+this->r->getDistance()/((routeSpeed<speed_object)?routeSpeed:speed_object);
         routeP.precedent = this->r->getCoordDepart();
-        std::for_each(accesible.beging(),accesible.end(),[routeP,&aHit](RoutePath &a){
+        std::for_each(accessible.beging(),accessible.end(),[routeP,&aHit](RoutePath &a){
                                                                                     if(a.r == routeP.r)
                                                                                     {
                                                                                         aHit = true;
@@ -50,14 +55,16 @@ bool RoutePath::addAccessible(std::vector<RoutePath>& accesible,float speed_obje
                                                                                         }
                                                                                      }
                                                                                 });
+        std::for_each(parcourue.beging(),parcourue.end(),[routeP,&aHit](RoutePath &a){if(a.r == routeP.r)aHit = true;});
         if(!aHit)
-            accesible.push_back(routeP);
+            accessible.push_back(routeP);
     }
 }
 
 void Dynamique::pathfinding(vec2<float> objectif)
 {
-    std::vector<RoutePath> accesible;
+    std::vector<RoutePath> accessible;
+    std::vector<RoutePath> parcourue;
     vec2<float> coord_actuel = this->getCoor();
     vec2<float> a;
     RoutePath plusProche{nullptr,UINT_MAX,coord_actuel};
@@ -73,7 +80,14 @@ void Dynamique::pathfinding(vec2<float> objectif)
         }
     }
     this->parcours.push_back(plusProche.r->getCoordDepart());
-    plusProche.addAccessible(accesible,this->max_speed);
+    plusProche.addAccessible(accessible,this->max_speed);
+    parcourue.push_back(plusProche);
+    while(accessible.size() > 0)
+    {
+        accessible.front().addAccessible(accessible,this->max_speed);
+        parcourue.push_back(accessible[0]);
+        accessible.erase(0);
+    }
 }
 
 vec2<float> Dynamique::getSpeed()
